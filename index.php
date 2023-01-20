5d06b9acd19980a8f79eb43c9c808854cdee47fa
Ahmed Shibani <sheipani@gmail.com>
2023-01-20 19:21:18 +0300

    Release version 1.0.0

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "App Server on \t\033[32m" . $_SERVER['SERVER_ADDR'] . "\033[0m \trunning version \033[33m 0.0.6 \033[0m\n";
+echo "App Server on \t\033[32m" . $_SERVER['SERVER_ADDR'] . "\033[0m \trunning version \033[33m 1.0.0 \033[0m\n";
 
 ?>
