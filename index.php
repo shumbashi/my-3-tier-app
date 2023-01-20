989ed373b497e7628c65a8e85b75b6b4464c8a41
Ahmed Shibani <sheipani@gmail.com>
2023-01-20 19:40:34 +0300

    Added different region colors

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 $region = getenv('REGION');
-echo "App Server on \033[32m" . $_SERVER['SERVER_ADDR'] . "\033[0m in \033[34m" . $region . "\033[0m region. Running version \033[33m 1.0.0 \033[0m\n";
+if ($region == "Tripoli") {
+    $colour = "\033[92m";
+} else {
+    $colour = "\033[34m";
+}
+echo "App Server on \033[32m" . $_SERVER['SERVER_ADDR'] . "\033[0m in " . $colour . $region . "\033[0m region. Running version \033[33m 1.0.0 \033[0m\n";
 
 ?>
