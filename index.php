373c0f550b9a9530ca86b27dc70e5dd0f9271896
Ahmed Shibani <sheipani@gmail.com>
2023-01-20 16:46:07 +0300

    Added tab

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "App Server on \033[32m" . $_SERVER['SERVER_ADDR'] . "\033[0m \trunning version \033[33m0.0.5\033[0m\n";
+echo "App Server on \t\033[32m" . $_SERVER['SERVER_ADDR'] . "\033[0m \trunning version \033[33m0.0.5\033[0m\n";
 
 ?>
