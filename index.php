ab49fa023f01086a67400c766a975132d64e1c4f
Ahmed Shibani <sheipani@gmail.com>
2023-01-23 12:40:21 +0300

    Release v2

--- index.php
+++ index.php
@@ -5,6 +5,6 @@ if ($region == "Tripoli") {
 } else {
     $colour = "\033[34m";
 }
-echo "App Server on \033[32m" . $_SERVER['SERVER_ADDR'] . "\033[0m in " . $colour . $region . "\033[0m region. Running version \033[33m 1.0.0 \033[0m\n";
+echo "App Server on \033[32m" . $_SERVER['SERVER_ADDR'] . "\033[0m in " . $colour . $region . "\033[0m region. Running version \033[33m 2.0.0 \033[0m\n";
 
 ?>
